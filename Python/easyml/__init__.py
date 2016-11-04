8903177d2eaeebc5889b5d279956c2a806ffae08
paulhendricks <paul.hendricks.2013@owu.edu>
2016-11-03 23:26:37 -0400

    added scaffolding for correlation_test

--- Python/easyml/__init__.py
+++ Python/easyml/__init__.py
@@ -2,9 +2,11 @@ from . import bootstrap
 from . import plot
 from . import process
 from . import sample
+from . import utils
 
 __all__ = []
 __all__ += bootstrap.__all__
 __all__ += plot.__all__
 __all__ += process.__all__
 __all__ += sample.__all__
+__all__ += utils.__all__
---
+++ Python/easyml/utils.py
@@ -0,0 +1,9 @@
+"""Utility functions.
+"""
+
+
+__all__ = ['correlation_test']
+
+
+def correlation_test():
+    return 1
