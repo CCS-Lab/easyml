1716eca6b84074d9d0d78db39da490a6ad277afe
paulhendricks <paul.hendricks.2013@owu.edu>
2016-11-06 19:17:29 -0500

    updated init

--- Python/easyml/__init__.py
+++ Python/easyml/__init__.py
@@ -1,5 +1,6 @@
 from . import bootstrap
 from . import datasets
+from . import factory
 from . import plot
 from . import sample
 from . import utils
@@ -7,6 +8,7 @@ from . import utils
 __all__ = []
 __all__ += bootstrap.__all__
 __all__ += datasets.__all__
+__all__ += factory.__all__
 __all__ += plot.__all__
 __all__ += sample.__all__
 __all__ += utils.__all__
