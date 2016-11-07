403dc3c021204ee39d51c47e38eb7489578f19b5
paulhendricks <paul.hendricks.2013@owu.edu>
2016-11-06 19:31:36 -0500

    added template for prostate analysis

---
+++ Python/examples/prostate/analysis.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+from easyml.factory import easy_glmnet
+
+
+if __name__ == "__main__":
+    # Load data
+    data = pd.read_table('./prostate.txt')
+
+    # Analyze data
+    easy_glmnet(data) # need to update constants and make them arguments
