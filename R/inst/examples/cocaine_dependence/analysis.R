68a9a3004c371d57e31898ea03d7c59b8f7f660b
paulhendricks <paul.hendricks.2013@owu.edu>
2016-11-18 19:01:31 -0500

    results close; seeding may make exact!

--- R/inst/examples/cocaine_dependence/analysis.R
+++ R/inst/examples/cocaine_dependence/analysis.R
@@ -4,6 +4,6 @@ library(easyml) # https://github.com/CCS-Lab/easyml
 data("cocaine_dependence", package = "easyml")
 
 # Analyze data
-easy_glmnet(cocaine_dependence, dependent_variable = "DIAGNOSIS",
+easy_glmnet(cocaine_dependence, "DIAGNOSIS", 
             family = "binomial", exclude_variables = c("subject", "AGE"), 
             categorical_variables = c("Male"))
