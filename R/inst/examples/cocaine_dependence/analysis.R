334a8760dfe6df79912010f1764399c6c7cfd116
paulhendricks <paul.hendricks.2013@owu.edu>
2016-11-19 09:51:23 -0500

    rerun with seed

--- R/inst/examples/cocaine_dependence/analysis.R
+++ R/inst/examples/cocaine_dependence/analysis.R
@@ -1,7 +1,7 @@
 library(easyml) # https://github.com/CCS-Lab/easyml
 
 # Load data
-data("cocaine_dependence", package = "easyml")
+data("cocaine_dependence", package = "easyml"); set.seed(43210)
 
 # Analyze data
 easy_glmnet(cocaine_dependence, "DIAGNOSIS", 
