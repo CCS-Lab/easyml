1e1c30ccdd8a9f92c0f951981ff2901c71942319
paulhendricks <paul.hendricks.2013@owu.edu>
2017-01-14 18:28:28 -0500

    updated travis file...let's see what happens

---
+++ tests/travis/run_test.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [ ${TASK} == "r_test" ]; then
+    # Set environment to exit with any non-zero exit codes
+    set -e
+    export _R_CHECK_TIMINGS_=0
+    
+    # Install R for OSx
+    wget https://cran.rstudio.com/bin/macosx/R-latest.pkg  -O /tmp/R-latest.pkg
+    sudo installer -pkg "/tmp/R-latest.pkg" -target /
+    
+    # Install devtools
+    Rscript -e "install.packages('devtools', repo = 'https://cran.rstudio.com')"
+    
+    # Install package dependencies
+    cd R
+    Rscript -e "library(devtools); library(methods); options(repos=c(CRAN='https://cran.rstudio.com')); install_deps(dependencies = TRUE)"
+    cd ..
+    
+    # Build package
+    R CMD INSTALL easyml_*.tar.gz
+    
+    # Run tests
+    cd R
+    Rscript -e "devtools::test()" || exit -1
+    Rscript tests/travis/r_vignettes.R
+    
+    # If successful this far, exit with exit code 0 (sucess)
+    exit 0
+fi
+
+if [ ${TASK} == "python_test" ]; then
+    # make all || exit -1
+    # # use cached dir for storing data
+    # rm -rf ${PWD}/data
+    # mkdir -p ${CACHE_PREFIX}/data
+    # ln -s ${CACHE_PREFIX}/data ${PWD}/data
+    # 
+    # if [ ${TRAVIS_OS_NAME} == "osx" ]; then
+    #     python -m nose tests/python/unittest || exit -1
+    #     python3 -m nose tests/python/unittest || exit -1
+    #     make cython3
+    #     # cython tests
+    #     export MXNET_ENFORCE_CYTHON=1
+    #     python3 -m nose tests/python/unittest || exit -1
+    #     python3 -m nose tests/python/train || exit -1
+    # else
+    #     nosetests tests/python/unittest || exit -1
+    #     nosetests3 tests/python/unittest || exit -1
+    #     nosetests3 tests/python/train || exit -1
+    # fi
+    exit 0
+fi
---
+++ tests/travis/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# if [ ${TRAVIS_OS_NAME} == "osx" ]; then
+#     brew update
+#     brew tap homebrew/science
+#     brew install opencv
+#     brew install python3
+#     brew install fftw
+#     brew install libpng
+#     brew install ImageMagick
+#     if [ ${TASK} == "python_test" ]; then
+#         python -m pip install --user nose numpy cython
+#         python3 -m pip install --user nose numpy cython
+#     fi
+# fi
