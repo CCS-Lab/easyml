124c6ba7126503d1edda2f43da5fa67835e8a77d
paulhendricks <paul.hendricks.2013@owu.edu>
2017-02-23 22:08:57 -0500

    updated test for preprocessing

--- R/tests/testthat/test-preprocess.R
+++ R/tests/testthat/test-preprocess.R
@@ -3,10 +3,50 @@ context("preprocess")
 
 test_that("Test preprocess_identity.", {
   expect_equal(preprocess_identity(identity), identity)
+  
+  actual <- preprocess_identity(identity, categorical_variables = c("a", "b"))
+  expect_equal(actual, identity)
 })
 
 test_that("Test preprocess_scale.", {
-  # set.seed(12345)
-  # preprocess_scale(list(X = mtcars))
-  # expect_equal()
+  # X
+  set.seed(12345)
+  actual <- preprocess_scale(list(X = mtcars))
+  expected <- list(X = data.frame(base::scale(mtcars)))
+  expect_equal(actual, expected)
+  
+  # X, with categorical_variables
+  set.seed(12345)
+  cat_vars <- c(rep(FALSE, 10), TRUE)
+  actual <- preprocess_scale(list(X = mtcars), 
+                             categorical_variables = cat_vars)
+  X <- base::scale(mtcars[, -11])
+  expected <- list(X = cbind(mtcars[, 11, drop = FALSE], data.frame(X)))
+  expect_equal(actual, expected)
+
+  # X_train and X_test
+  actual <- preprocess_scale(list(X_train = mtcars[1:25, ], 
+                                  X_test = mtcars[26:32, ]))
+  
+  X_train <- base::scale(mtcars[1:25, ])
+  X_test <- base::scale(mtcars[26:32, ], 
+                        attr(X_train, "scaled:center"), 
+                        attr(X_train, "scaled:scale"))
+  expected <- list(X_train = data.frame(X_train), 
+                   X_test = data.frame(X_test))
+  expect_equal(actual, expected)
+  
+  # X_train and X_test, with categorical_variables
+  cat_vars <- c(rep(FALSE, 10), TRUE)
+  actual <- preprocess_scale(list(X_train = mtcars[1:25, ], 
+                                  X_test = mtcars[26:32, ]), 
+                             categorical_variables = cat_vars)
+  
+  X_train <- base::scale(mtcars[1:25, -11])
+  X_test <- base::scale(mtcars[26:32, -11], 
+                        attr(X_train, "scaled:center"), 
+                        attr(X_train, "scaled:scale"))
+  expected <- list(X_train = cbind(mtcars[1:25, 11, drop = FALSE], data.frame(X_train)), 
+                   X_test = cbind(mtcars[26:32, 11, drop = FALSE], data.frame(X_test)))
+  expect_equal(actual, expected)
 })
