6a3bbe2698f7b37075ce0938b21f1ecdd7cdc0b0
paulhendricks <paul.hendricks.2013@owu.edu>
2017-02-26 15:22:47 -0500

    attempt to get linux build

--- tests/travis/run_test.sh
+++ tests/travis/run_test.sh
@@ -7,10 +7,6 @@ if [ ${TASK} == "r_test" ]; then
     # Not sure what this export does
     export _R_CHECK_TIMINGS_=0
     
-    # Install R for OSx
-    wget https://cran.rstudio.com/bin/macosx/R-latest.pkg  -O /tmp/R-latest.pkg
-    sudo installer -pkg "/tmp/R-latest.pkg" -target /
-    
     # Install devtools and roxygen2
     Rscript -e "install.packages(c('roxygen2', 'devtools'), repo = 'https://cran.rstudio.com')"
     
@@ -36,16 +32,6 @@ if [ ${TASK} == "r_test" ]; then
 fi
 
 if [ ${TASK} == "python_test" ]; then
-    # Update and upgrade brew
-    brew update
-    
-    # Reinstall gcc (since it doesn't work for some reason)
-    brew reinstall gcc
-    
-    # Install Python for OSx
-    brew tap homebrew/science
-    brew install python3
-    
     # Install package
     cd Python/
     pip3 install .
--- tests/travis/setup.sh
+++ tests/travis/setup.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+if [ ${TRAVIS_OS_NAME} == "osx" ]; then
+    if [ ${TASK} == "r_test" ]; then
+    
+    # Install R for OSx
+    wget https://cran.rstudio.com/bin/macosx/R-latest.pkg  -O /tmp/R-latest.pkg
+    sudo installer -pkg "/tmp/R-latest.pkg" -target /
+    
+    fi
+    
+    if [ ${TASK} == "python_test" ]; then
+    
+    # Update and upgrade brew
+    brew update
+    
+    # Reinstall gcc (since it doesn't work for some reason)
+    brew reinstall gcc
+
+    
+    # Install Python for OSx
+    brew tap homebrew/science
+    brew install python3
+    
+    fi
+fi
