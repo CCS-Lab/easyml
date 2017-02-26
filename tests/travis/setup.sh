96f8b6a227d979af37115ee9e2c901770b94010c
paulhendricks <paul.hendricks.2013@owu.edu>
2017-02-26 15:39:34 -0500

    added section for linux-specific installation instructions

--- tests/travis/setup.sh
+++ tests/travis/setup.sh
@@ -24,3 +24,23 @@ if [ ${TRAVIS_OS_NAME} == "osx" ]; then
     
     fi
 fi
+
+if [ ${TRAVIS_OS_NAME} == "linux" ]; then
+    # update apt-get
+    apt-get update
+    
+    if [ ${TASK} == "r_test" ]; then
+    
+    # Install R for linux
+    apt-get install r-base
+    
+    fi
+    
+    if [ ${TASK} == "python_test" ]; then
+    
+    # Install Python for OSx
+    apt-get install python-pip python3-pip python3-dev python3-virtualenv python3-tk
+    pip3 install --upgrade pip
+    
+    fi
+fi
