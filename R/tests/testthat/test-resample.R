ae7ac94a2454a9076c7a61e5ae9c0d75a2086c0b
paulhendricks <paul.hendricks.2013@owu.edu>
2017-02-26 13:08:26 -0500

    updated docs

--- R/tests/testthat/test-resample.R
+++ R/tests/testthat/test-resample.R
@@ -2,9 +2,10 @@ library(easyml)
 context("resample")
 
 test_that("Test resample_simple_train_test_split.", {
-  expect_equal(1, 1)
 })
 
 test_that("Test resample_stratified_train_test_split.", {
-  expect_equal(1, 1)
+})
+
+test_that("Test resample_fold_train_test_split.", {
 })
