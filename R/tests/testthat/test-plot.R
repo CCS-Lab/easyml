d06515057207ee704baa8d4d6d201666446f5c27
paulhendricks <paul.hendricks.2013@owu.edu>
2017-03-04 16:27:17 -0500

    some more quick tests

--- R/tests/testthat/test-plot.R
+++ R/tests/testthat/test-plot.R
@@ -13,16 +13,26 @@ test_that("Test plot_predictions_gaussian.", {
 })
 
 test_that("Test plot_predictions_binomial.", {
+  g <- plot_predictions_binomial(rbinom(100, 1, 0.25), rbinom(100, 1, 0.25))
+  expect_equal(class(g), c("gg", "ggplot"))
 })
 
 test_that("Test plot_metrics_gaussian_mean_squared_error.", {
+  g <- plot_metrics_gaussian_mean_squared_error(rnorm(100))
+  expect_equal(class(g), c("gg", "ggplot"))
 })
 
 test_that("Test plot_metrics_gaussian_r2_score.", {
+  g <- plot_metrics_gaussian_r2_score(runif(100))
+  expect_equal(class(g), c("gg", "ggplot"))
 })
 
 test_that("Test plot_metrics_gaussian_cor_score.", {
+  g <- plot_metrics_gaussian_cor_score(runif(100))
+  expect_equal(class(g), c("gg", "ggplot"))
 })
 
 test_that("Test plot_metrics_binomial_area_under_curve.", {
+  g <- plot_metrics_binomial_area_under_curve(runif(100))
+  expect_equal(class(g), c("gg", "ggplot"))
 })
