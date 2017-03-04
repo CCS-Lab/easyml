72eb8c7c9b9b5b8d9beeff1fab648fc4a4442649
paulhendricks <paul.hendricks.2013@owu.edu>
2017-03-04 16:30:32 -0500

    added some other quick plot tests

--- R/tests/testthat/test-plot.R
+++ R/tests/testthat/test-plot.R
@@ -1,10 +1,41 @@
 library(easyml)
 context("plot")
 
+# Load data
+data("cocaine_dependence", package = "easyml")
+
+# Settings
+.n_samples <- 10
+.n_divisions <- 10
+.n_iterations <- 2
+
+# Analyze data
+glmnet_results <- easy_glmnet(cocaine_dependence, "diagnosis", 
+                              family = "binomial", 
+                              resample = resample_stratified_class_train_test_split, 
+                              preprocess = preprocess_scale, 
+                              exclude_variables = c("subject"), 
+                              categorical_variables = c("male"), 
+                              n_samples = .n_samples, n_divisions = .n_divisions, 
+                              n_iterations = .n_iterations, random_state = 12345, n_core = 1, 
+                              alpha = 1, nlambda = 200)
+
 test_that("Test plot_coefficients_processed.", {
+  coefficients_processed <- process_coefficients(glmnet_results$coefficients)
+  g <- plot_coefficients_processed(coefficients_processed)
+  expect_equal(class(g), c("gg", "ggplot"))
 })
 
+# Analyze data
+random_forest_results <- easy_random_forest(cocaine_dependence, "diagnosis", 
+                                            family = "binomial", exclude_variables = c("subject"), 
+                                            categorical_variables = c("male"), 
+                                            n_samples = .n_samples, n_divisions = .n_divisions, 
+                                            n_iterations = .n_iterations, random_state = 1, n_core = 1)
+
 test_that("Test plot_variable_importances.", {
+  g <- plot_variable_importances(random_forest_results$variable_importances)
+  expect_equal(class(g), c("gg", "ggplot"))
 })
 
 test_that("Test plot_predictions_gaussian.", {
