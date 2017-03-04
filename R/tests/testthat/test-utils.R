c0fbf4af7cb3b1aaf342c0200e54aff8e88591ec
paulhendricks <paul.hendricks.2013@owu.edu>
2017-03-04 17:27:19 -0500

    another test

--- R/tests/testthat/test-utils.R
+++ R/tests/testthat/test-utils.R
@@ -17,27 +17,27 @@ test_that("Test correlation_test.", {
   expect_true(nrow(m$p_value) == ncol(m$p_value))
 })
 
-# Load data
-data("cocaine_dependence", package = "easyml")
-cocaine_dependence <- cocaine_dependence
-
-# Settings
-.n_samples <- 10
-.n_divisions <- 10
-.n_iterations <- 2
-
-# Analyze data
-glmnet_results <- easy_glmnet(cocaine_dependence, "diagnosis", 
-                              family = "binomial", 
-                              resample = resample_stratified_class_train_test_split, 
-                              preprocess = preprocess_scale, 
-                              exclude_variables = c("subject"), 
-                              categorical_variables = c("male"), 
-                              n_samples = .n_samples, n_divisions = .n_divisions, 
-                              n_iterations = .n_iterations, random_state = 12345, n_core = 1, 
-                              alpha = 1, nlambda = 200)
-
-test_that("Test process_coefficients.", {
-  coefficients_processed <- process_coefficients(glmnet_results$coefficients)
-  expect_equal(class(coefficients_processed), c("data.frame"))
-})
+# # Load data
+# data("cocaine_dependence", package = "easyml")
+# cocaine_dependence <- cocaine_dependence
+# 
+# # Settings
+# .n_samples <- 10
+# .n_divisions <- 10
+# .n_iterations <- 2
+# 
+# # Analyze data
+# glmnet_results <- easy_glmnet(cocaine_dependence, "diagnosis", 
+#                               family = "binomial", 
+#                               resample = resample_stratified_class_train_test_split, 
+#                               preprocess = preprocess_scale, 
+#                               exclude_variables = c("subject"), 
+#                               categorical_variables = c("male"), 
+#                               n_samples = .n_samples, n_divisions = .n_divisions, 
+#                               n_iterations = .n_iterations, random_state = 12345, n_core = 1, 
+#                               alpha = 1, nlambda = 200)
+# 
+# test_that("Test process_coefficients.", {
+#   coefficients_processed <- process_coefficients(glmnet_results$coefficients)
+#   expect_equal(class(coefficients_processed), c("data.frame"))
+# })
