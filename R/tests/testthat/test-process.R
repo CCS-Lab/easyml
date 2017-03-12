078cbf01946387723b1e03b67ad00f82681bdd17
paulhendricks <paul.hendricks.2013@owu.edu>
2017-03-11 20:39:39 -0500

    fixed process bug; templates for some tests

---
+++ R/tests/testthat/test-process.R
@@ -0,0 +1,10 @@
+library(easyml)
+context("process")
+
+test_that("Test process_variable_importances.", {
+  expect_equal(1, 1)
+})
+
+test_that("Test process_coefficients.", {
+  expect_equal(1, 1)
+})
