0da9aefac183f285db9a6c4a902d3868779511f8
paulhendricks <paul.hendricks.2013@owu.edu>
2017-04-08 16:23:00 -0400

    added vignettes...starting to work on prostate and cocaine

--- R/inst/data-raw/save.R
+++ R/inst/data-raw/save.R
@@ -1,8 +1,10 @@
+# cocaine dependence
 cocaine_dependence <- read.table("./inst/data-raw/cocaine_dependence.txt", header = TRUE, 
                                  stringsAsFactors = FALSE)
 colnames(cocaine_dependence) <- tolower(colnames(cocaine_dependence))
 save(cocaine_dependence, file = "./data/cocaine_dependence.rda")
 
+# prostate
 prostate <- read.table("./inst/data-raw/prostate.txt", header = TRUE, 
                       stringsAsFactors = FALSE)
 save(prostate, file = "./data/prostate.rda")
