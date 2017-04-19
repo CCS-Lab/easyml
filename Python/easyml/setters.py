ea8ceac80ca9001c506aca61054e5a09ddf36c3f
paulhendricks <paul.hendricks.2013@owu.edu>
2017-04-19 00:03:16 -0400

    some minor fixes to setters

--- Python/easyml/setters.py
+++ Python/easyml/setters.py
@@ -1,8 +1,9 @@
-"""TO BE EDITED.
-"""
+'''TO BE EDITED.
+'''
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+from . import measure as meas
 from . import preprocess as prep
 from . import resample as res
 
@@ -11,6 +12,7 @@ __all__ = []
 
 
 def set_parallel(n_core):
+    n_core = int(n_core)
     if n_core == 1:
         parallel = False
     elif n_core > 1:
@@ -22,16 +24,16 @@ def set_parallel(n_core):
 def set_column_names(column_names, dependent_variable,
                      exclude_variables=None, preprocess=None, categorical_variables=None):
     column_names = [c for c in column_names if c != dependent_variable]
-    if exclude_variables is not None:
+    if exclude_variables:
         column_names = [c for c in column_names if c not in exclude_variables]
-    if categorical_variables is not None and preprocess is prep.preprocess_scale:
+    if categorical_variables and preprocess is prep.preprocess_scale:
         column_names = [c for c in column_names if c not in categorical_variables]
         column_names = categorical_variables + column_names
     return column_names
 
 
 def set_categorical_variables(column_names, categorical_variables=None):
-    if categorical_variables is not None:
+    if categorical_variables:
         categorical_variables = [True if c in categorical_variables else False for c in column_names]
     return categorical_variables
 
@@ -43,10 +45,10 @@ def set_random_state(random_state=None):
 
 
 def set_resample(resample=None, family=None):
-    if resample is None:
-        if family == "gaussian":
+    if not resample:
+        if family == 'gaussian':
             resample = train_test_split
-        elif family == "binomial":
+        elif family == 'binomial':
             resample = res.resample_equal_proportion
         else:
             raise ValueError
@@ -54,12 +56,19 @@ def set_resample(resample=None, family=None):
 
 
 def set_preprocess(preprocess=None):
-    if preprocess is None:
+    if not preprocess:
         preprocess = prep.preprocess_identity
     return preprocess
 
 
-def set_measure(measure=None, algorithm=None, family=None):
+def set_measure(measure=None, family=None):
+    if not measure:
+        if family == 'gaussian':
+            measure = 1
+        elif family == 'binomial':
+            measure = 1
+        else:
+            raise ValueError
     return measure
 
 
@@ -72,5 +81,12 @@ def set_independent_variables(data, dependent_variable):
     X = data.drop(dependent_variable, axis=1).values
     return X
 
-def set_plot_metrics(algorithm=None, family=None):
-    return None
+
+def set_plot_metrics(measure):
+    if measure is 1:
+        plot_metrics = 1
+    elif measure is 2:
+        plot_metrics = 2
+    else:
+        raise ValueError
+    return plot_metrics
