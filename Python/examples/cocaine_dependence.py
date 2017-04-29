d16f334d42529f73c0d33cd1d77f3354061b1b04
paulhendricks <paul.hendricks.2013@owu.edu>
2017-04-29 13:46:51 -0400

    updated examples to be one liners

--- Python/examples/cocaine_dependence.py
+++ Python/examples/cocaine_dependence.py
@@ -1,50 +1,31 @@
-import matplotlib.pyplot as plt
-import os
+from easyml import glmnet, random_forest, support_vector_machine
 import pandas as pd
 
-from easyml import plot
-from easyml.glmnet import easy_glmnet
-from easyml.random_forest import easy_random_forest
-from easyml.support_vector_machine import easy_support_vector_machine
 
+# Load data
+cocaine_dependence = pd.read_table('./Python/examples/cocaine_dependence.txt')
 
-# Set matplotlib settings
-plt.style.use('ggplot')
+# Analyze data
+output = glmnet.easy_glmnet(cocaine_dependence, 'DIAGNOSIS',
+                            family='binomial',
+                            exclude_variables=['subject'],
+                            categorical_variables=['Male'],
+                            random_state=12345, progress_bar=True, n_core=1,
+                            n_samples=5, n_divisions=5, n_iterations=2,
+                            model_args={'alpha': 1, 'n_lambda': 200})
 
-if __name__ == '__main__':
-    # Load data
-    directory = './Python/examples/'
-    cocaine_dependence = pd.read_table(os.path.join(directory, 'cocaine_dependence.txt'))
+# Analyze data
+output = random_forest.easy_random_forest(cocaine_dependence, 'DIAGNOSIS',
+                                          family='binomial',
+                                          exclude_variables=['subject'],
+                                          categorical_variables=['Male'],
+                                          random_state=1, progress_bar=True, n_core=1,
+                                          n_samples=5, n_divisions=5, n_iterations=2,
+                                          model_args={'n_estimators': 10})
 
-    # Analyze data
-    output = easy_glmnet(cocaine_dependence, 'DIAGNOSIS',
-                         family='binomial',
-                         exclude_variables=['subject'],
-                         categorical_variables=['Male'],
-                         random_state=12345, progress_bar=True, n_core=1,
-                         n_samples=5, n_divisions=5, n_iterations=2,
-                         model_args={'alpha': 1, 'n_lambda': 200})
-    print(output.estimator)
-    plot.plot_coefficients_processed(output.coefficients)
-    print(output.column_names)
-
-
-
-    # Analyze data
-    output = easy_random_forest(cocaine_dependence, 'DIAGNOSIS',
-                                family='binomial',
-                                exclude_variables=['subject'],
-                                categorical_variables=['Male'],
-                                random_state=1, progress_bar=True, n_core=1,
-                                n_samples=5, n_divisions=5, n_iterations=2,
-                                model_args={'n_estimators': 10})
-
-    output = easy_support_vector_machine(cocaine_dependence, 'DIAGNOSIS',
-                                         family='binomial',
-                                         exclude_variables=['subject'],
-                                         categorical_variables=['Male'],
-                                         random_state=1, progress_bar=True, n_core=1,
-                                         n_samples=5, n_divisions=5, n_iterations=2)
-    plot.plot_metrics_binomial_area_under_curve(output.metrics_train)
-    plot.plot_metrics_binomial_area_under_curve(output.metrics_test)
-    plot.plot_predictions_binomial(output.predictions_train_mean)
+output = support_vector_machine.easy_support_vector_machine(cocaine_dependence, 'DIAGNOSIS',
+                                                            family='binomial',
+                                                            exclude_variables=['subject'],
+                                                            categorical_variables=['Male'],
+                                                            random_state=1, progress_bar=True, n_core=1,
+                                                            n_samples=5, n_divisions=5, n_iterations=2)
--- Python/examples/prostate.py
+++ Python/examples/prostate.py
@@ -1,33 +1,23 @@
-import matplotlib.pyplot as plt
-import os
+from easyml import glmnet, random_forest, support_vector_machine
 import pandas as pd
 
-from easyml.glmnet import easy_glmnet
-from easyml.random_forest import easy_random_forest
-from easyml.support_vector_machine import easy_support_vector_machine
 
+# Load data
+prostate = pd.read_table('./Python/examples/prostate.txt')
 
-# Set matplotlib settings
-plt.style.use('ggplot')
+# Analyze data
+output = glmnet.easy_glmnet(prostate, 'lpsa',
+                            random_state=1, progress_bar=True, n_core=1,
+                            n_samples=10, n_divisions=10, n_iterations=2,
+                            model_args={'alpha': 1})
 
-if __name__ == '__main__':
-    # Load data
-    directory = './Python/examples/'
-    prostate = pd.read_table(os.path.join(directory, 'prostate.txt'))
+# Analyze data
+output = random_forest.easy_random_forest(prostate, 'lpsa',
+                                          random_state=1, progress_bar=True, n_core=1,
+                                          n_samples=5, n_divisions=5, n_iterations=2,
+                                          model_args={'n_estimators': 10})
 
-    # Analyze data
-    output = easy_glmnet(prostate, 'lpsa',
-                         random_state=1, progress_bar=True, n_core=1,
-                         n_samples=10, n_divisions=10, n_iterations=2,
-                         model_args={'alpha': 1})
-
-    # Analyze data
-    output = easy_random_forest(prostate, 'lpsa',
-                                random_state=1, progress_bar=True, n_core=1,
-                                n_samples=5, n_divisions=5, n_iterations=2,
-                                model_args={'n_estimators': 10})
-
-    # Analyze data
-    output = easy_support_vector_machine(prostate, 'lpsa',
-                                         random_state=1, progress_bar=True, n_core=1,
-                                         n_samples=5, n_divisions=5, n_iterations=2)
+# Analyze data
+output = support_vector_machine.easy_support_vector_machine(prostate, 'lpsa',
+                                                            random_state=1, progress_bar=True, n_core=1,
+                                                            n_samples=5, n_divisions=5, n_iterations=2)
