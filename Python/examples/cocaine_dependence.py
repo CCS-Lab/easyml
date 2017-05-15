f8d8024f711eb544beaa1abd12e6c5a10bad7b8b
paulhendricks <paul.hendricks.2013@owu.edu>
2017-05-15 09:01:24 -0400

    playing around with example codes

--- Python/examples/cocaine_dependence.py
+++ Python/examples/cocaine_dependence.py
@@ -1,4 +1,6 @@
 from easyml.glmnet import easy_glmnet
+from easyml import random_forest, support_vector_machine
+
 
 # Load data
 import pandas as pd
@@ -13,25 +15,27 @@ results = easy_glmnet(cocaine_dependence, 'DIAGNOSIS',
                       n_samples=5, n_divisions=5, n_iterations=2,
                       model_args={'alpha': 1, 'n_lambda': 200})
 print(results.plot_coefficients())
+
+# Analyze data
+results = random_forest.easy_random_forest(cocaine_dependence, 'DIAGNOSIS',
+                                          family='binomial',
+                                          exclude_variables=['subject'],
+                                          categorical_variables=['Male'],
+                                          random_state=1, progress_bar=True, n_core=1,
+                                          n_samples=5, n_divisions=5, n_iterations=2,
+                                          model_args={'n_estimators': 10})
+print(results.plot_variable_importances())
+
+results = support_vector_machine.easy_support_vector_machine(cocaine_dependence, 'DIAGNOSIS',
+                                                            family='binomial',
+                                                            exclude_variables=['subject'],
+                                                            categorical_variables=['Male'],
+                                                            random_state=1, progress_bar=True, n_core=1,
+                                                            n_samples=5, n_divisions=5, n_iterations=2)
+
 print(results.plot_predictions_single_train_test_split_train())
 print(results.plot_predictions_single_train_test_split_test())
 print(results.plot_roc_single_train_test_split_train())
 print(results.plot_roc_single_train_test_split_test())
 print(results.plot_model_performance_train())
 print(results.plot_model_performance_test())
-
-# # Analyze data
-# results = random_forest.easy_random_forest(cocaine_dependence, 'DIAGNOSIS',
-#                                           family='binomial',
-#                                           exclude_variables=['subject'],
-#                                           categorical_variables=['Male'],
-#                                           random_state=1, progress_bar=True, n_core=1,
-#                                           n_samples=5, n_divisions=5, n_iterations=2,
-#                                           model_args={'n_estimators': 10})
-#
-# results = support_vector_machine.easy_support_vector_machine(cocaine_dependence, 'DIAGNOSIS',
-#                                                             family='binomial',
-#                                                             exclude_variables=['subject'],
-#                                                             categorical_variables=['Male'],
-#                                                             random_state=1, progress_bar=True, n_core=1,
-#                                                             n_samples=5, n_divisions=5, n_iterations=2)
--- Python/examples/prostate.py
+++ Python/examples/prostate.py
@@ -10,14 +10,21 @@ output = glmnet.easy_glmnet(prostate, 'lpsa',
                             random_state=1, progress_bar=True, n_core=1,
                             n_samples=10, n_divisions=10, n_iterations=2,
                             model_args={'alpha': 1})
+output.plot_coefficients()
 
 # Analyze data
 output = random_forest.easy_random_forest(prostate, 'lpsa',
                                           random_state=1, progress_bar=True, n_core=1,
                                           n_samples=5, n_divisions=5, n_iterations=2,
                                           model_args={'n_estimators': 10})
+output.plot_variable_importances()
 
 # Analyze data
 output = support_vector_machine.easy_support_vector_machine(prostate, 'lpsa',
                                                             random_state=1, progress_bar=True, n_core=1,
                                                             n_samples=5, n_divisions=5, n_iterations=2)
+
+output.plot_predictions_single_train_test_split_train()
+output.plot_predictions_single_train_test_split_test()
+output.plot_model_performance_train()
+output.plot_model_performance_test()
