c8e4a7e23afeee770603e3affbde3b063d43d4dc
paulhendricks <paul.hendricks.2013@owu.edu>
2017-06-04 20:42:29 -0400

    updated with example for no random state

--- Python/examples/example2.py
+++ Python/examples/example2.py
@@ -12,12 +12,21 @@ X, y = prostate.drop('lpsa', axis=1).values, prostate['lpsa'].values
 sclr = StandardScaler()
 X_preprocessed = sclr.fit_transform(X)
 
+# no random state
+coefficients = []
+for i in range(10):
+    model = ElasticNet(alpha=1, standardize=False, cut_point=0.0, n_lambda=200)
+    print(id(model))
+    model.fit(X_preprocessed, y)
+    coefficients.append(np.asarray(model.coef_))
+print(coefficients)
+
 # seed set at outer level
 np.random.seed(43210)
 coefficients = []
 for i in range(10):
-    print(id(model))
     model = ElasticNet(alpha=1, standardize=False, cut_point=0.0, n_lambda=200)
+    print(id(model))
     model.fit(X_preprocessed, y)
     coefficients.append(np.asarray(model.coef_))
 print(coefficients)
@@ -26,8 +35,8 @@ print(coefficients)
 coefficients = []
 for i in range(10):
     np.random.seed(43210)
-    print(id(model))
     model = ElasticNet(alpha=1, standardize=False, cut_point=0.0, n_lambda=200)
+    print(id(model))
     model.fit(X_preprocessed, y)
     coefficients.append(np.asarray(model.coef_))
 print(coefficients)
@@ -35,9 +44,9 @@ print(coefficients)
 # seed set at function level
 coefficients = []
 for i in range(10):
-    print(id(model))
     random_state = np.random.RandomState(i)
     model = ElasticNet(alpha=1, standardize=False, cut_point=0.0, n_lambda=200, random_state=random_state)
+    print(id(model))
     model.fit(X_preprocessed, y)
     coefficients.append(np.asarray(model.coef_))
 print(coefficients)
@@ -45,8 +54,8 @@ print(coefficients)
 coefficients = []
 random_state = np.random.RandomState(43210)
 for i in range(10):
-    print(id(model))
     model = ElasticNet(alpha=1, standardize=False, cut_point=0.0, n_lambda=200, random_state=random_state)
+    print(id(model))
     model.fit(X_preprocessed, y)
     coefficients.append(np.asarray(model.coef_))
 print(coefficients)
