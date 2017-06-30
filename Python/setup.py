485e950dfde9e89ec235380175bd4a675f95bd22
paulhendricks <paul.hendricks.2013@owu.edu>
2017-06-30 17:19:41 -0400

    modified setup.py file

--- Python/setup.py
+++ Python/setup.py
@@ -1,22 +1,12 @@
-from setuptools import setup, find_packages
-from codecs import open
-from os import path
-
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+from setuptools import setup
 
 setup(
     name='easymlpy',
     version='0.1.0',
     description='A Python toolkit for easily building and evaluating machine learning models.',
-    long_description=long_description,
     url='https://github.com/CCS-Lab/easyml',
     author='OSU CCSL',
-    author_email='https://github.com/CCS-Lab/',
+    author_email='paul.hendricks.2013@owu.edu',
     license='MIT',
     keywords='easy machine learning glmnet penalized regression random forest penalized regression',
     packages=['easymlpy'],
